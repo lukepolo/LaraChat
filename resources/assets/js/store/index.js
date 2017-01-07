223436563b255454f46beb898f023fa3ab512aac
Luke Policinski <Luke@LukePOLO.com>
2017-01-07 18:14:28 -0500

    removing unsed files

--- resources/assets/js/store/index.js
+++ resources/assets/js/store/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import userStore from './modules/UserStore'
 import messageStore from './modules/MessageStore'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    userStore,
     messageStore
   }
 })
