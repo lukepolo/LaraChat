56fda742c641f8ae240142e3f254a80eba773c25
Luke Policinski <Luke@LukePOLO.com>
2017-01-07 18:19:14 -0500

    views

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -1,40 +1,11 @@
 <template>
     <section>
-        <div>
-            <ul>
-                <li v-for="message in messages">
-                    <p>{{ message.message }}</p>
-                    {{ message.user.name }}
-                </li>
-            </ul>
-        </div>
-        <input type="text" @keypress.enter="sendMessage" v-model="message">
+        You are logged in in
     </section>
 </template>
 
 <script>
     export default {
-        created() {
-          this.$store.dispatch('getMessages')
-        },
-        data() {
-            return {
-                message : null
-            }
-        },
-        methods: {
-            sendMessage() {
-                this.$store.dispatch('sendMessage', this.message).then((message) => {
-                    if(message) {
-                        this.message = null
-                    }
-                })
-            }
-        },
-        computed: {
-            messages() {
-                return this.$store.state.messageStore.messages;
-            }
-        }
+
     }
-</script>
+</script>d
