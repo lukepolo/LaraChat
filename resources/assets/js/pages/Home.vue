577ab6b1f1d0b2371cfe557bd717df8b231eaa67
Luke Policinski <luke@lukepolo.com>
2017-01-10 21:30:04 -0500

    adding defaults

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -1,11 +1,48 @@
 <template>
     <section>
-        You are logged in in
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Messages
+                    </div>
+                    <div class="panel-body">
+                        <ul>
+                            <li>
+                                <div>username</div>
+                                <p>message</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="input-group">
+                            <input class="form-control" type="text">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">Send Message!</button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
     export default {
+        created() {
 
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
     }
 </script>
