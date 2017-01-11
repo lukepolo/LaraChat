42e7c27837d16618de89d38b70c435e1ea87718d
Luke Policinski <luke@lukepolo.com>
2017-01-10 21:25:36 -0500

    styles added

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -1,14 +1,30 @@
 <template>
     <section>
-        <div>
-            <ul>
-                <li v-for="message in messages">
-                    <p>{{ message.message }}</p>
-                    {{ message.user.name }}
-                </li>
-            </ul>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Messages
+                    </div>
+                    <div class="panel-body">
+                        <ul>
+                            <li v-for="message in messages">
+                                <div>{{ message.user.name }}</div>
+                                <p>{{ message.message }}</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="input-group">
+                            <input class="form-control" type="text" @keypress.enter="sendMessage" v-model="message" ref="message">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button" @click="sendMessage">Send Message!</button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <input type="text" @keypress.enter="sendMessage" v-model="message">
     </section>
 </template>
 
@@ -16,6 +32,9 @@
     export default {
         created() {
           this.$store.dispatch('getMessages')
+            Vue.nextTick(() => {
+                this.$refs.message.focus()
+            });
         },
         data() {
             return {
@@ -33,6 +52,13 @@
         },
         computed: {
             messages() {
+                Vue.nextTick(() => {
+                    const container = $(this.$el).find('.panel-body')[0]
+                    if(container) {
+                        container.scrollTop = container.scrollHeight
+                    }
+                })
+
                 return this.$store.state.messageStore.messages;
             }
         }
